aa43a0b14640b6ecd98e061d00fd34f3f7524a40
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-03-29 18:42:50 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,17 +7,21 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
+  num1/num2
 end
 
 def multiplication(num1, num2)
+  num1*num2
 end
 
 def modulo(num1, num2)
+  num1%num2
 end
 
 def square_root(num)
+  Math.square_root(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  num1 + (( num2 * num3 ) / num4 )
 end
